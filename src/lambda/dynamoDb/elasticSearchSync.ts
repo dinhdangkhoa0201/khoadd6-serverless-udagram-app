8687b317aaf00580ee2f01826f6abfe5e6b96345
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-09-01 12:19:00 +0700

    DynamoDB Stream

--- src/lambda/dynamoDb/elasticSearchSync.ts
+++ src/lambda/dynamoDb/elasticSearchSync.ts
@@ -3,4 +3,8 @@ import "source-map-support/register"
 
 export const handler : DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
     console.log("Processing events batch from DynamoDB", JSON.stringify(event));
+
+    for (const record of event.Records) {
+        console.log("Processing record", JSON.stringify(record));
+    }
 }
